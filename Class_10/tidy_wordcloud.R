df4e8c71f0fe904e5348513c3a395b3e57e013c8
David Joseph Wrisley <dwrisley@gmail.com>
2019-02-27 23:51:13 +0400

    moved in class 10 scripts

---
+++ Class_10/tidy_wordcloud.R
@@ -0,0 +1,52 @@
+
+# adapted from https://richpauloo.github.io/2017-12-29-Using-tidytext-to-make-word-clouds/
+
+path <- '/Users/djw12/Downloads/data/Lippert_Elders.txt'  #try Elders2 as well
+dat <- read.table(path, header = FALSE, fill = TRUE)  #take txt file and read it into a table
+
+#all packages must be loaded in using install.packages("nameofpackage")
+
+library(dplyr) # for data wrangling
+library(tidytext) # for NLP
+library(stringr) # to deal with strings
+library(wordcloud) # to render wordclouds
+library(RColorBrewer) # to set colors
+library(knitr) # for tables
+library(DT) # for dynamic tables
+library(tidyr) # for data tidying
+library(reshape2) # for data tidying
+library(ggplot2) # for creating visualizations
+
+tidy_dat <- tidyr::gather(dat, key, word) %>% select(word) # DF into a column
+tidy_dat$word %>% length() # check length
+
+unique(tidy_dat$word) %>% length() #look for the unique tokens
+
+# tokenizing
+
+tokens <- tidy_dat %>%
+  unnest_tokens(word, word) %>%
+  dplyr::count(word, sort = TRUE) %>%
+  ungroup()
+
+# removing stop words
+
+tokens_clean <- tokens %>%
+  anti_join(stop_words)
+
+#define palette for word cloud, visualize wordcloud
+
+pal <- brewer.pal(8,"Dark2")
+tokens_clean %>%
+  with(wordcloud(word, n, random.order = FALSE, max.words = 150, colors=pal))
+
+# remove some custom stop words
+
+uni_sw <- data.frame(word = c("james","zane","ralph", "harry"))
+tokens_clean <- tokens_clean %>%
+  anti_join(uni_sw, by = "word")
+
+# visualize the word cloud
+
+tokens_clean %>%
+  with(wordcloud(word, n, random.order = FALSE, max.words = 150, colors=pal))
