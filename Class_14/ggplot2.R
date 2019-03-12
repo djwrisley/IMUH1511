7a2b8003c2b300ccd198ec229fd3efdea7bc93f8
David Joseph Wrisley <dwrisley@gmail.com>
2019-03-12 10:47:45 +0400

    adding class 14 materials

---
+++ Class_14/ggplot2.R
@@ -0,0 +1,22 @@
+# trying the built in dataset iris
+
+install.packages("dplyr")
+install.packages("ggplot2")
+library(dplyr)
+iris
+library(ggplot2)
+ggplot(data=iris, aes(x=Sepal.Length, y=Sepal.Width, color=Species)) + geom_point(size=2)
+
+# trying another dataset 
+# https://github.com/awesomedata/awesome-public-datasets/tree/master/Datasets
+
+setwd("/Users/djw12/Downloads/")
+titanic <- read.csv("FIFA.csv")
+ggplot(data=titanic, aes(x=date, y=attendees)) + geom_point(size=2) 
+
+# visualize the trend in the data as a linear line
+
+setwd("/Users/djw12/Downloads/")
+titanic <- read.csv("FIFA.csv")
+ggplot(data=titanic, aes(x=date, y=attendees)) + geom_point(size=2) + geom_smooth(method = lm, se = TRUE)
+
