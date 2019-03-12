eb282c37b49ee387b80599ed5d85c2fdaf2b772f
David Wrisley <dwrisley@gmail.com>
2019-03-12 10:49:52 +0400

    distinguishing titanic from FIFA datasets

--- Class_14/ggplot2.R
+++ Class_14/ggplot2.R
@@ -7,8 +7,14 @@ iris
 library(ggplot2)
 ggplot(data=iris, aes(x=Sepal.Length, y=Sepal.Width, color=Species)) + geom_point(size=2)
 
-# trying another dataset 
+# trying the Titanic dataset
 # https://github.com/awesomedata/awesome-public-datasets/tree/master/Datasets
+
+setwd("/Users/djw12/Downloads/")
+titanic <- read.csv("titanic.csv")
+ggplot(data=titanic, aes(x=date, y=attendees)) + geom_point(size=2) 
+
+# trying the FIFA dataset
 
 setwd("/Users/djw12/Downloads/")
 titanic <- read.csv("FIFA.csv")
